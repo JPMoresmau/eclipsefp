713f90e58ca6df8c501682be3fb9d38028f45310
thiago.arrais <thiago.arrais@gmail.com>
2006-01-13 17:13:01 +0100

    First implementation of the getNextLocation for CompilationUnits
    darcs-hash:20060113161301-49d33-533d6753e4513a95b8ee907453491273807f0ed7.gz

--- net.sf.eclipsefp.haskell.core.jparser/src/net/sf/eclipsefp/haskell/core/jparser/ast/CompilationUnit.java
+++ net.sf.eclipsefp.haskell.core.jparser/src/net/sf/eclipsefp/haskell/core/jparser/ast/CompilationUnit.java
@@ -3,6 +3,7 @@ package net.sf.eclipsefp.haskell.core.jparser.ast;
 import org.eclipse.core.resources.IFile;
 
 import de.leiffrenzel.fp.haskell.core.halamo.ICompilationUnit;
+import de.leiffrenzel.fp.haskell.core.halamo.IHaskellLanguageElement;
 import de.leiffrenzel.fp.haskell.core.halamo.IModule;
 import de.leiffrenzel.fp.haskell.core.halamo.ISourceLocation;
 
@@ -18,9 +19,46 @@ public class CompilationUnit implements ICompilationUnit {
 		return new IModule[] { fModule };
 	}
 
-	public ISourceLocation getNextLocation(ISourceLocation srcLoc) {
-		// TODO Auto-generated method stub
+	public ISourceLocation getNextLocation(final ISourceLocation srcLoc) {
+		//special case for empty files
+		if (isEmptyUntitledModule(fModule)
+		 && 0 >= srcLoc.getLine()
+		 && 0 >= srcLoc.getColumn())
+		{
+			return new SourceLocation(1, 0);
+		}
+		
+		ISourceLocation result = searchNextLocation(new IModule[] {fModule},
+				srcLoc);
+		if (result == null) {
+			searchNextLocation(fModule.getExportSpecifications(), srcLoc);
+		}
+		if (result == null) {
+			result = searchNextLocation(fModule.getImports(), srcLoc);
+		}
+		if (result == null) {
+			result = searchNextLocation(fModule.getDeclarations(), srcLoc);
+		}
+		return result;
+	}
+	
+	private <T extends IHaskellLanguageElement> ISourceLocation
+		searchNextLocation(T[] elems, final ISourceLocation srcLoc)
+	{
+		for (T elem : elems) {
+			final ISourceLocation expLoc = elem.getSourceLocation();
+			if (expLoc.isAfter(srcLoc)) {
+				return expLoc;
+			}
+		}
 		return null;
+	}
+
+	private boolean isEmptyUntitledModule(IModule module) {
+		return "".equals(module.getName())
+		    && 0 == module.getDeclarations().length
+		    && 0 == module.getExportSpecifications().length
+		    && 0 == module.getImports().length;
 	}
 
 	public Object getAdapter(Class adapter) {
