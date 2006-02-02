1ad4f6a93660b74c3886c272df083c256b14a794
thiago.arrais <thiago.arrais@gmail.com>
2006-02-02 18:36:59 +0100

    Adjusted post installation checker to look for source code plugin
    darcs-hash:20060202173659-49d33-ab76bd2d126a98e33f9d2a6306bfe41ce9227f67.gz

--- net.sf.eclipsefp.haskell.build.check/src/net/sf/eclipsefp/haskell/build/check/VerifyPostInstallationConditions.java
+++ net.sf.eclipsefp.haskell.build.check/src/net/sf/eclipsefp/haskell/build/check/VerifyPostInstallationConditions.java
@@ -1,5 +1,8 @@
 package net.sf.eclipsefp.haskell.build.check;
 
+import org.eclipse.core.runtime.IExtension;
+import org.eclipse.core.runtime.IExtensionPoint;
+import org.eclipse.core.runtime.IExtensionRegistry;
 import org.eclipse.core.runtime.IPlatformRunnable;
 import org.eclipse.core.runtime.Platform;
 
@@ -8,6 +11,7 @@ public class VerifyPostInstallationConditions implements IPlatformRunnable {
 	public Object run(Object args) throws Exception {
 		try {
 			checkPluginExistence("de.leiffrenzel.fp.haskell.core");
+			checkSourceCodePlugin("de.leiffrenzel.fp.haskell.source");
 			return 0;
 		} catch (Exception e) {
 			final String message = "Bad installation. " + e.getMessage();
@@ -19,8 +23,19 @@ public class VerifyPostInstallationConditions implements IPlatformRunnable {
 	private void checkPluginExistence(final String pluginName) throws Exception {
 		Object bundle = Platform.getBundle(pluginName);
 		if (bundle == null) {
-			throw new Exception("Haskell core plugin not found.");
+			throw new Exception(pluginName + " plugin not found.");
 		}
+	}
+
+	private void checkSourceCodePlugin(final String pluginName) throws Exception {
+		IExtensionRegistry reg = Platform.getExtensionRegistry();
+		IExtensionPoint point = reg.getExtensionPoint("org.eclipse.pde.core.source");
+		for (IExtension ext : point.getExtensions()) {
+			if (pluginName.equals(ext.getNamespace())) {
+				return;
+			}
+		}
+		throw new Exception("Source code plugin " + pluginName + " not found");
 	}
 
 }
