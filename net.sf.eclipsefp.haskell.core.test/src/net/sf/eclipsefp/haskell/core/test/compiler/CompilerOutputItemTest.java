0d8d638d13a5ee8618f421a8498ad0bbdb8c47c0
thiago.arrais <thiago.arrais@gmail.com>
2006-02-03 13:37:17 +0100

    Added testes for the compiler output item addToComment method
    darcs-hash:20060203123717-49d33-4f625965eb48845aeea9895ce1d613624aa47534.gz

---
+++ net.sf.eclipsefp.haskell.core.test/src/net/sf/eclipsefp/haskell/core/test/compiler/CompilerOutputItemTest.java
@@ -0,0 +1,19 @@
+package net.sf.eclipsefp.haskell.core.test.compiler;
+
+import de.leiffrenzel.fp.haskell.core.compiler.CompilerOutputItem;
+import junit.framework.TestCase;
+
+public class CompilerOutputItemTest extends TestCase {
+	
+	public void testAddComment() {
+		CompilerOutputItem createdItem = new CompilerOutputItem("test.hs", 1, "0:");
+		createdItem.addToComment("Comment line 1");
+		createdItem.addToComment("Comment line 2");
+		
+		final String expectedComment = "0:\n" +
+	                                   "Comment line 1\n" +
+	                                   "Comment line 2";
+		assertEquals(expectedComment, createdItem.getComment());
+	}
+	
+}
