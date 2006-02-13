5c3ad1cc9dcf8fbc0bb0adb06dd8adceb6327a15
thiago.arrais <thiago.arrais@gmail.com>
2006-02-13 19:42:34 +0100

    Removed the old parser code
    darcs-hash:20060213184234-49d33-7beb27cb48f4796ac1523ea340b18eecbafa4aa3.gz

--- net.sf.eclipsefp.haskell.core.parser.win32/INTERNAL/conf/build.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-
-ghc -fglasgow-exts -c ParserImpl.hs 
-ghc -I%JAVA_HOME%/include -I%JAVA_HOME%/include/win32 -optc -mno-cygwin -c bridge.c
-ghc --mk-dll -optdll --add-stdcall-alias -o nativeparser.dll ParserImpl.o ParserImpl_stub.o bridge.o -package base -package haskell-src
-copy nativeparser.dll ..\os\win32\x86\
-del *.dll *.o *.hi *stub.c *stub.h
-echo Done
