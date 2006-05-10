dbe565ac0ff930dc34429ef7f9f392816f9cb226
thiago.arrais <thiago.arrais@gmail.com>
2006-05-10 16:35:47 +0200

    Added easymock for mocking objects
    EasyMock is already being used on the HaskellCorePluginTest, but we need
    to gradully convert other tests to use it.
    
    EclipseFP's developer SDK is getting quite large. There are at least two
    Eclipse plugins already (antlr and easymock). Maybe we should package this as
    a feature or something.
    
    darcs-hash:20060510143547-49d33-e40b59ee1e1d1e1356b0d6800ed1ff103fb35b5b.gz

--- net.sf.eclipsefp.haskell.core.test/src/net/sf/eclipsefp/haskell/core/test/HaskellCorePluginTest.java
+++ net.sf.eclipsefp.haskell.core.test/src/net/sf/eclipsefp/haskell/core/test/HaskellCorePluginTest.java
@@ -1,22 +1,33 @@
 package net.sf.eclipsefp.haskell.core.test;
 
 import net.sf.eclipsefp.haskell.core.HaskellCorePlugin;
-import net.sf.eclipsefp.haskell.core.test.internal.doubles.MockWorkspace;
 import net.sf.eclipsefp.haskell.core.test.internal.doubles.StubBundleContext;
 
+import org.eclipse.core.resources.IResourceChangeListener;
+import org.eclipse.core.resources.IWorkspace;
+import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.Plugin;
 
 import junit.framework.TestCase;
+import static org.easymock.EasyMock.*;
 
 public class HaskellCorePluginTest extends TestCase {
 	
 	public void testRegistersResourceChangeListener() throws Exception {
-		//TODO use a mock library here
-		MockWorkspace workspace = new MockWorkspace();
+		IWorkspace workspace = createMock(IWorkspace.class);
+		
+		workspace.addResourceChangeListener((IResourceChangeListener) anyObject(), anyInt());
+		expectLastCall().times(1);
+		
+		expect(workspace.getRoot()).
+			andReturn(ResourcesPlugin.getWorkspace().getRoot());
+		
+		replay(workspace);
+		
 		Plugin corePlugin = new HaskellCorePlugin(workspace);
 		corePlugin.start(new StubBundleContext());
 		
-		workspace.verify();
+		verify(workspace);
 	}
 
 }
