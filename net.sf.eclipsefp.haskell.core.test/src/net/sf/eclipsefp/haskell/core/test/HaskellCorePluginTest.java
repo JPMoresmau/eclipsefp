3c6a57f4d46f1adf51e9c2dd85d5b064cc0b4b6e
thiago.arrais <thiago.arrais@gmail.com>
2006-05-10 16:59:02 +0200

    Small refactoring to replace stub classes with mocks
    darcs-hash:20060510145902-49d33-3ccce0fad25657cdf9542589c61e888fc0cbdc1c.gz

--- net.sf.eclipsefp.haskell.core.test/src/net/sf/eclipsefp/haskell/core/test/HaskellCorePluginTest.java
+++ net.sf.eclipsefp.haskell.core.test/src/net/sf/eclipsefp/haskell/core/test/HaskellCorePluginTest.java
@@ -1,33 +1,50 @@
 package net.sf.eclipsefp.haskell.core.test;
 
 import net.sf.eclipsefp.haskell.core.HaskellCorePlugin;
-import net.sf.eclipsefp.haskell.core.test.internal.doubles.StubBundleContext;
 
 import org.eclipse.core.resources.IResourceChangeListener;
 import org.eclipse.core.resources.IWorkspace;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.Plugin;
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleContext;
 
 import junit.framework.TestCase;
 import static org.easymock.EasyMock.*;
 
 public class HaskellCorePluginTest extends TestCase {
 	
+	private BundleContext fStubContext = createNiceMock(BundleContext.class);
+
 	public void testRegistersResourceChangeListener() throws Exception {
 		IWorkspace workspace = createMock(IWorkspace.class);
-		
 		workspace.addResourceChangeListener((IResourceChangeListener) anyObject(), anyInt());
 		expectLastCall().times(1);
 		
 		expect(workspace.getRoot()).
 			andReturn(ResourcesPlugin.getWorkspace().getRoot());
 		
-		replay(workspace);
+		replay(workspace, getStubContext());
 		
 		Plugin corePlugin = new HaskellCorePlugin(workspace);
-		corePlugin.start(new StubBundleContext());
+		corePlugin.start(getStubContext());
 		
 		verify(workspace);
+	}
+
+	@Override
+	protected void setUp() throws Exception {
+		Bundle stubBundle = createNiceMock(Bundle.class);
+		expect(stubBundle.getSymbolicName()).
+			andReturn("net.sf.eclipsefp.haskell.core.test.StubBundle").
+			anyTimes();
+		expect(getStubContext().getBundle()).
+			andReturn(stubBundle);
+		replay(stubBundle);
+	}
+
+	private BundleContext getStubContext() {
+		return fStubContext;
 	}
 
 }
