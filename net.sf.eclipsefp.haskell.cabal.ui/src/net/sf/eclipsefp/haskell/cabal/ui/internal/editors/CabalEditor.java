c37f649a83280727bc2e57f693d6943830bea456
Leif Frenzel <himself@leiffrenzel.de>
2006-06-10 11:01:49 +0200

    added basic text editor skeleton
    darcs-hash:20060610090149-34f1c-7d3362cdcadadc8e5535b00837f9d9fe03784903.gz

---
+++ net.sf.eclipsefp.haskell.cabal.ui/src/net/sf/eclipsefp/haskell/cabal/ui/internal/editors/CabalEditor.java
@@ -0,0 +1,16 @@
+// Copyright (c) 2006 by Leif Frenzel <himself@leiffrenzel.de>
+// All rights reserved.
+package net.sf.eclipsefp.haskell.cabal.ui.internal.editors;
+
+import org.eclipse.ui.editors.text.TextEditor;
+
+/** <p>an editor for Cabal package description files.</p> 
+  * 
+  * <p>Note: this class is declared in <code>plugin.xml</code>.</p>
+  *
+  * @author Leif Frenzel
+  */
+public class CabalEditor extends TextEditor {
+
+
+}
