4819e9b929f55e005659081d6c646084935d4b71
jp <jp@moresmau.fr>
2006-11-29 13:18:14 +0100

    Hugs Executable Check
    Checks for the presence of the Hugs executable entered by the user and displays the version number
    
    darcs-hash:20061129121814-01564-6edab583ce52028cbda9428613e68f60db0fb853.gz

--- net.sf.eclipsefp.haskell.hugs/src/net/sf/eclipsefp/haskell/hugs/ui/preferences/GeneralTab.java
+++ net.sf.eclipsefp.haskell.hugs/src/net/sf/eclipsefp/haskell/hugs/ui/preferences/GeneralTab.java
@@ -1,6 +1,11 @@
 // Copyright (c) 2003-2005 by Leif Frenzel - see http://leiffrenzel.de
 package net.sf.eclipsefp.haskell.hugs.ui.preferences;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
 import net.sf.eclipsefp.common.ui.dialog.ExecutableDialogField;
 import net.sf.eclipsefp.common.ui.dialog.IDialogFieldListener;
 import net.sf.eclipsefp.common.ui.preferences.Tab;
@@ -28,10 +33,55 @@ public class GeneralTab extends Tab implements IHugsPreferenceNames {
   public Control createControl( final Composite parent ) {
     String labelText = "HUGS executable";
     ExecutableDialogField dlgField = new ExecutableDialogField( parent, 
-                                                                labelText ) {
+                                                                labelText ){
+    	
       protected String createDisplayContent( final String info ) {
-        // TODO
-        return "Executable check not implemented.";
+    	  try {
+    		  	File f=new File(info);
+    		  	if (f.exists() && f.canRead()){
+			        Process p=Runtime.getRuntime().exec(info);
+			        
+			        InputStream is=p.getInputStream();
+			        // read all text given by the console till the Hugs> prompt
+			        // this rely on the > at the prompt
+			        int r=is.read();
+			        while (((char)r)!='>' && r>-1){
+			        	r=is.read();
+			        }
+			        // read first space
+			        is.read();
+			        OutputStream os=p.getOutputStream();
+			        // write version command
+			        os.write((":version"+System.getProperty("line.separator","\n")).getBytes());
+			        os.flush();
+			        StringBuffer sb=new StringBuffer();
+			        r=is.read();
+			        // read everything till the first line return
+			        while (((char)r)!='\n' && ((char)r)!='\r' && r>-1){
+			        	sb.append((char)r);
+			        	r=is.read();
+			        }
+			        // read the rest
+			        while (is.available()>0){
+			        	is.read();
+			        }
+			        // quit
+			        os.write((":quit"+System.getProperty("line.separator","\n")).getBytes());
+			        os.flush();
+			        os.close();
+			        is.close();
+			        // the version, usually starting with --
+			        String version=sb.toString();
+			        if (version.startsWith("--")){
+			        	version=version.substring(2).trim();
+			        }
+			        return version;
+    		  	}
+    		  	return "Not found";
+    	  } catch (IOException ioe){
+    		  ioe.printStackTrace();
+    		  return ioe.getMessage();
+    	  }
       }
     };
 
