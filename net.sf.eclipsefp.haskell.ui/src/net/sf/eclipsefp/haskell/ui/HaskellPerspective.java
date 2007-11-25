425c509b453f598ca3140980885bf10a238b4529
Leif Frenzel <himself@leiffrenzel.de>
2007-11-25 21:27:47 +0100

    changed the perspective layout to use the Project Explorer instead of Resource Navigator and Module Browser
    darcs-hash:20071125202747-34f1c-81c3036f9b37d0a9cd745ac44a34b7cbb159a6a5.gz

--- net.sf.eclipsefp.haskell.ui/src/net/sf/eclipsefp/haskell/ui/HaskellPerspective.java
+++ net.sf.eclipsefp.haskell.ui/src/net/sf/eclipsefp/haskell/ui/HaskellPerspective.java
@@ -1,42 +1,83 @@
-// Copyright (c) 2003-2005 by Leif Frenzel - see http://leiffrenzel.de
+// Copyright (c) 2003-2007 by Leif Frenzel - see http://leiffrenzel.de
 package net.sf.eclipsefp.haskell.ui;
 
-import net.sf.eclipsefp.common.ui.FPPerspective;
-
+import net.sf.eclipsefp.haskell.ui.wizards.NewModuleWizard;
+import org.eclipse.debug.ui.IDebugUIConstants;
 import org.eclipse.ui.IFolderLayout;
 import org.eclipse.ui.IPageLayout;
+import org.eclipse.ui.IPerspectiveFactory;
+import org.eclipse.ui.console.IConsoleConstants;
 
-import net.sf.eclipsefp.haskell.ui.views.mbview.ModuleBrowser;
-import net.sf.eclipsefp.haskell.ui.wizards.NewModuleWizard;
 
-
-/** <p>The perspective for Haskell development.</p> 
-  * 
+/** <p>The perspective for Haskell development.</p>
+  *
   * @author Leif Frenzel
   */
-public class HaskellPerspective extends FPPerspective {
+public class HaskellPerspective implements IPerspectiveFactory {
 
-  @Override
-  protected void defineActions( final IPageLayout layout ) {
-    // defines common layout from the FP perspectives superclass
-    super.defineActions( layout );
-    // Add to "Open Perspective" menu    
+
+  // interface methods of IPerspectiveFactory
+  ///////////////////////////////////////////
+
+  public void createInitialLayout( final IPageLayout layout ) {
+    defineActions( layout );
+    defineLayout( layout );
+  }
+
+
+  // helping functions
+  ////////////////////
+
+  private void defineActions( final IPageLayout layout ) {
+    // Add to "New" menu
+    addNewShortcuts( layout );
+    // Add to "Show View" menu
+    layout.addShowViewShortcut( "org.eclipse.ui.navigator.ProjectExplorer" );
+    layout.addShowViewShortcut( IPageLayout.ID_BOOKMARKS );
+    layout.addShowViewShortcut( IPageLayout.ID_PROBLEM_VIEW );
+    layout.addShowViewShortcut( IPageLayout.ID_PROP_SHEET );
+    layout.addShowViewShortcut( IPageLayout.ID_TASK_LIST );
+    layout.addShowViewShortcut( IPageLayout.ID_OUTLINE );
+    layout.addShowViewShortcut( IConsoleConstants.ID_CONSOLE_VIEW );
+    // Add toolbar and menu actions
+    layout.addActionSet( IDebugUIConstants.LAUNCH_ACTION_SET );
+    // Add to "Open Perspective" menu
     layout.addPerspectiveShortcut( HaskellPerspective.class.getName() );
-    // add to "Show View" menu
-    layout.addShowViewShortcut( ModuleBrowser.ID );
-//    layout.addShowViewShortcut( ModuleDependenciesView.ID );
   }
-  
-  @Override
-  protected void addLeftViews( final IFolderLayout left ) {
-    super.addLeftViews( left );
-    left.addView( ModuleBrowser.ID );
-//    left.addView( ModuleDependenciesView.ID );
+
+  private void defineLayout( final IPageLayout layout ) {
+    String editorArea = layout.getEditorArea();
+
+    IFolderLayout left = layout.createFolder( "left",
+                                              IPageLayout.LEFT,
+                                              0.26f,
+                                              editorArea );
+    addLeftViews( left );
+    IFolderLayout bottom = layout.createFolder( "bottom",
+                                                IPageLayout.BOTTOM,
+                                                0.74f,
+                                                editorArea );
+    addBottomViews( bottom );
+    IFolderLayout right = layout.createFolder( "right",
+                                               IPageLayout.RIGHT,
+                                               0.80f,
+                                               editorArea );
+    right.addView( IPageLayout.ID_OUTLINE );
   }
-  
-  @Override
-  protected void addNewShortcuts( final IPageLayout layout ) {
+
+  private void addLeftViews( final IFolderLayout left ) {
+    left.addView( "org.eclipse.ui.navigator.ProjectExplorer" );
+  }
+
+  private void addBottomViews( final IFolderLayout bottom ) {
+    bottom.addView( IPageLayout.ID_TASK_LIST );
+    bottom.addView( IPageLayout.ID_PROBLEM_VIEW );
+    bottom.addView( IConsoleConstants.ID_CONSOLE_VIEW );
+  }
+
+  private void addNewShortcuts( final IPageLayout layout ) {
+    layout.addNewWizardShortcut( "org.eclipse.ui.wizards.new.folder" );
+    layout.addNewWizardShortcut( "org.eclipse.ui.wizards.new.file" );
     layout.addNewWizardShortcut( NewModuleWizard.ID );
-    super.addNewShortcuts( layout );
   }
 }
