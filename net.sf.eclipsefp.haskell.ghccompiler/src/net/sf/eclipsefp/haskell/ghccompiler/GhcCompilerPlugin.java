c4eb2d949243a1e16e8b57631826b82956a3b46f
Leif Frenzel <himself@leiffrenzel.de>
2008-02-16 15:47:51 +0100

    trivial - simplified trace
    darcs-hash:20080216144751-34f1c-302f3ff6df568a890297d8d3c48c581c208bc7b3.gz

--- net.sf.eclipsefp.haskell.ghccompiler/src/net/sf/eclipsefp/haskell/ghccompiler/GhcCompilerPlugin.java
+++ net.sf.eclipsefp.haskell.ghccompiler/src/net/sf/eclipsefp/haskell/ghccompiler/GhcCompilerPlugin.java
@@ -9,52 +9,52 @@ import org.eclipse.ui.plugin.AbstractUIPlugin;
 
 /** <p>The main plugin class to be used in the desktop.</p>
   *
-  * @author the mighty PDE wizard 
+  * @author the mighty PDE wizard
   */
 public class GhcCompilerPlugin extends AbstractUIPlugin {
 
   // The shared instance.
   private static GhcCompilerPlugin plugin;
-  
+
   public GhcCompilerPlugin() {
     plugin = this;
   }
-  
+
   /** <p>returns the shared instance.</p> */
   public static GhcCompilerPlugin getDefault() {
     return plugin;
   }
-  
+
   /** <p>returns the unique identifier for the GhcCompilerPlugin.</p> */
   public static String getPluginId() {
     return getDefault().getBundle().getSymbolicName();
   }
 
   public static boolean isTracing() {
-    String option = getPluginId() + "/trace/compiling";
+    String option = getPluginId() + "/compiling";
     String value = Platform.getDebugOption( option );
     return value != null && value.equals( "true" );
   }
-  
+
 
   // logging
   //////////
-  
+
   public static void log( final String message, final int severity ) {
-    Status status = new Status( severity, 
-                                getPluginId(), 
-                                IStatus.OK, 
-                                message, 
+    Status status = new Status( severity,
+                                getPluginId(),
+                                IStatus.OK,
+                                message,
                                 null );
     getDefault().getLog().log( status );
   }
 
   public static void log( final String message, final Throwable throwable ) {
-    Status status = new Status( IStatus.ERROR, 
-                                getPluginId(), 
-                                IStatus.OK, 
-                                message, 
+    Status status = new Status( IStatus.ERROR,
+                                getPluginId(),
+                                IStatus.OK,
+                                message,
                                 throwable );
     getDefault().getLog().log( status );
-  } 
+  }
 }
