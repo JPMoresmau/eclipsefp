8d68217110b2c973c5eb07251ae7d0f86c1e24c3
Leif Frenzel <himself@leiffrenzel.de>
2008-04-20 00:54:36 +0200

    minor cleanups
    darcs-hash:20080419225436-34f1c-7a516e785c97f2caa3de7e395a17e35053439c15.gz

--- net.sf.eclipsefp.haskell.ui/src/net/sf/eclipsefp/haskell/ui/internal/editors/haskell/text/CodeFolding.java
+++ net.sf.eclipsefp.haskell.ui/src/net/sf/eclipsefp/haskell/ui/internal/editors/haskell/text/CodeFolding.java
@@ -1,3 +1,6 @@
+// Copyright (c) 2008 by Leif Frenzel - see http://leiffrenzel.de
+// This code is made available under the terms of the Eclipse Public License,
+// version 1.0 (EPL). See http://www.eclipse.org/legal/epl-v10.html
 package net.sf.eclipsefp.haskell.ui.internal.editors.haskell.text;
 
 import java.util.ArrayList;
@@ -8,33 +11,13 @@ import de.leiffrenzel.cohatoe.server.core.CohatoeServer;
 
 public class CodeFolding implements ICodeFolding {
 
-  // TODO: this implementation has been generated to comply with the
-  // ICodeFolding interface. If you change the interface, you
-  // will probably have to adjust this file to your changes.
-
-  // This implementation class is mostly for marshalling parameters that
-  // come from Java code into strings that can be sent over to the Haskell
-  // side, where they are unmarshalled and passed to the actual Haskell
-  // code. The interface ICodeFolding is used for obtaining a function
-  // instance and executing the function. Typically, code on the Java
-  // side that uses your Haskell function will look like this:
-  //
-  //    // get our function from the factory
-  //    CohatoeServer server = CohatoeServer.getInstance();
-  //    ICodeFolding codeFolding = server.createFunction( ICodeFolding.class );
-  //    if( codeFolding != null ) {
-  //      // talk to Haskell by calling it
-  //      String result = codeFolding.performCodeFolding( "myParam" );
-  //      TODO do sth. with result
-  //    }
-
   public List<ICodeFoldingRegion> performCodeFolding( final String param ) {
     List<ICodeFoldingRegion> result = new ArrayList<ICodeFoldingRegion>();
     String[] params = new String[] { param };
     CohatoeServer server = CohatoeServer.getInstance();
     try {
       String[] retVal = server.evaluate( ICodeFolding.class, params );
-      umarshal( retVal, result );
+      unmarshal( retVal, result );
     } catch( CohatoeException cex ) {
       HaskellUIPlugin.log( cex );
     }
@@ -45,8 +28,8 @@ public class CodeFolding implements ICodeFolding {
   // helping functions
   ////////////////////
 
-  private void umarshal( final String[] retVal,
-                         final List<ICodeFoldingRegion> result ) {
+  private void unmarshal( final String[] retVal,
+                          final List<ICodeFoldingRegion> result ) {
     if( retVal != null ) {
       int index = 0;
       while( retVal.length > index + 1 ) {
--- net.sf.eclipsefp.haskell.ui/src/net/sf/eclipsefp/haskell/ui/internal/editors/haskell/text/ICodeFolding.java
+++ net.sf.eclipsefp.haskell.ui/src/net/sf/eclipsefp/haskell/ui/internal/editors/haskell/text/ICodeFolding.java
@@ -1,7 +1,14 @@
+// Copyright (c) 2008 by Leif Frenzel - see http://leiffrenzel.de
+// This code is made available under the terms of the Eclipse Public License,
+// version 1.0 (EPL). See http://www.eclipse.org/legal/epl-v10.html
 package net.sf.eclipsefp.haskell.ui.internal.editors.haskell.text;
 
 import java.util.List;
 
+/** <p>interface to the Haskell code that computes folding regions.</p>
+  *
+  * @author Leif Frenzel
+  */
 public interface ICodeFolding {
 
   public interface ICodeFoldingRegion {
