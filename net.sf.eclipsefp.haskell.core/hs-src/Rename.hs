31b12cec488ddaeeda07768c293d7d77762e3b22
[Author not set in preferences] <ttencate@gmail.com>
2008-04-19 20:04:46 +0200

    1208628268170 transport containers for refactor/rename
    darcs-hash:20080419180446-ad432-5d75bd41fb3901002695e333d334ad3984820b0d.gz

--- net.sf.eclipsefp.haskell.core/hs-src/Rename.hs
+++ net.sf.eclipsefp.haskell.core/hs-src/Rename.hs
@@ -1,4 +1,12 @@
+{-# language DisambiguateRecordFields #-}
+
 module Rename where
+
+import RenameInfo
+import ReplaceEdit
+import SrcLoc
+
+
 
 -- We must import Cohatoe.API and implement resource so that this code
 -- can be dynamically loaded as plugin.
@@ -8,7 +16,20 @@ resource = plugin {
   pluginMain = performRename
 }
 
+
+     	        
+
+
+
 -- TODO: Here's what we actually do when someone calls us.
 performRename :: [String] -> IO [String]
-performRename args = undefined
+performRename = fmap marshal . refactor . unmarshal 
 
+unmarshal :: [ String ] -> RenameInfo
+unmarshal = undefined
+
+marshal :: [ ReplaceEdit ] -> [ String ]
+marshal = undefined
+
+refactor :: RenameInfo -> IO [ ReplaceEdit ]
+refactor = undefined
