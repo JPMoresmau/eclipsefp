e112bff28e97b159f672d09dc09ff7a63b6835b5
eclipse <eclipse@henning-thielemann.de>
2008-04-20 12:30:18 +0200

    narrowed type signature in expression tree
    darcs-hash:20080420103018-34ce8-d4c353dbe0766a9c5afd8c48b2a700a401803b03.gz

--- net.sf.eclipsefp.haskell.ui/hs-src/TypeSignature.hs
+++ net.sf.eclipsefp.haskell.ui/hs-src/TypeSignature.hs
@@ -9,9 +9,13 @@ import Data.Maybe (mapMaybe, )
 
 import System.FilePath ((</>))
 
+-- import PprCore (pprType)
+import TypeRep (pprType, )
+import qualified Var
 import HsExpr (pprFunBind, )
 import HsBinds (LHsBinds, HsBind(..), ppr_monobind, pprPrag, fun_infix, fun_matches, )
-import Outputable (showSDocUnqual, OutputableBndr, BindingSite(LetBind), (<+>), ppr, pprBndr, nest, vcat, text, )
+import Outputable (showSDocUnqual, OutputableBndr, BindingSite(LetBind),
+          (<+>), ppr, pprBndr, nest, vcat, hcat, text, dcolon, )
 import SrcLoc (Located(L), spans, unLoc, )
 
 
@@ -26,13 +30,19 @@ getTypeSignature srcRoot fileName srcLoc =
       -- return (moduleFileName)
       return (findTypeSignature typeCheckedMod srcLoc)
 
-findTypeSignature :: (OutputableBndr id) =>
-   LHsBinds id ->   -- Bag (Located (HsBinds.HsBind id)) ->
+findTypeSignature :: -- (OutputableBndr id) =>
+   LHsBinds Var.Var ->   -- Bag (Located (HsBinds.HsBind id)) ->
    (Int, Int) ->
    String
 findTypeSignature typeCheckedMod srcLoc =
    concatMap (\ (AbsBinds _ _ exports _) -> showSDocUnqual $
-       vcat [pprBndr LetBind x | (_,x,_,_) <- exports]) $
+       vcat [pprTypedBinder x
+--       vcat [pprBndr LetBind x -- (Var.varType x)
+                 | (_,x,_,_) <- exports]) $
+{-
+   concatMap (\ (AbsBinds _ _ exports _) -> showSDocUnqual $
+       vcat [text "|-|" <+> pprBndr LetBind x <+> text "|-|" | (_,x,_,_) <- exports]) $
+-}
 --   concatMap (\ (AbsBinds _ _ exps _) -> showSDocUnqual $ vcat $ map ppr_exp exps) $
 --   map (\ (AbsBinds _ _ _ _) -> 'a') $
 --   map (\ (VarBind _ _) -> 'a') $
@@ -47,9 +57,19 @@ findTypeSignature typeCheckedMod srcLoc =
            then Just bind
            else Nothing) $
    bagToList typeCheckedMod
+
+
+pprTypedBinder binder =
+  if Var.isTyVar binder
+    then text "@" <+> pprTyVarBndr binder
+    else pprIdBndr binder <+> dcolon <+> pprType (Var.idType binder)
+
+pprTyVarBndr = error "eclipsefp.pprTyVarBndr not implemented"
+
+pprIdBndr id = ppr id
+
+
 {-
-  where
-    ppr_exp (tvs, gbl, lcl, prags)
-	= vcat [ppr gbl <+>  ppr tvs <+> ppr lcl,
-	  	nest 2 (vcat (map (pprPrag gbl) prags))]
+Example call:
+Control.Monad.Error.runErrorT $ getTypeSignature "/data2/programming/haskell/ide/eclipsefp2/net.sf.eclipsefp.haskell.ui/hs-src" "TypeSignature.hs" (36,10) >>= Control.Monad.Trans.liftIO . putStrLn
 -}
