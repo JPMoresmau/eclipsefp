d81218baddee5dc7f8b2a2173ac50a6da446b860
Thomas ten Cate <ttencate@gmail.com>
2009-06-29 17:35:44 +0200

    Made hovers over squiggly error/warning text show the error/warning.
    Previously, this would just do the thing-at-point lookup,
    whereas hovering over the icon in the gutter would give the error/warning message.

--- net.sf.eclipsefp.haskell.ui/src/net/sf/eclipsefp/haskell/ui/internal/editors/haskell/HaskellTextHover.java
+++ net.sf.eclipsefp.haskell.ui/src/net/sf/eclipsefp/haskell/ui/internal/editors/haskell/HaskellTextHover.java
@@ -3,6 +3,7 @@
 // version 1.0 (EPL). See http://www.eclipse.org/legal/epl-v10.html
 package net.sf.eclipsefp.haskell.ui.internal.editors.haskell;
 
+import java.util.Iterator;
 import net.sf.eclipsefp.haskell.scion.client.Scion;
 import net.sf.eclipsefp.haskell.scion.commands.ThingAtPointCommand;
 import net.sf.eclipsefp.haskell.scion.types.Location;
@@ -14,7 +15,9 @@ import org.eclipse.jface.text.DefaultTextHover;
 import org.eclipse.jface.text.IDocument;
 import org.eclipse.jface.text.IRegion;
 import org.eclipse.jface.text.ITextViewer;
+import org.eclipse.jface.text.Position;
 import org.eclipse.jface.text.source.Annotation;
+import org.eclipse.jface.text.source.IAnnotationModel;
 import org.eclipse.jface.text.source.ISourceViewer;
 
 class HaskellTextHover extends DefaultTextHover {
@@ -30,43 +33,49 @@ class HaskellTextHover extends DefaultTextHover {
   @Override
   public String getHoverInfo( final ITextViewer textViewer,
                               final IRegion hoverRegion ) {
-	  /*
-    String result = null;
-    CohatoeServer server = CohatoeServer.getInstance();
-    IEditorTextHover fun = server.createFunction( IEditorTextHover.class );
-    if( fun != null ) {
-      try {
-        IDocument doc = textViewer.getDocument();
-        SrcLoc loc = SrcLoc.fromDocOffset( doc, hoverRegion.getOffset() );
-        IFile file = findFile();
-        if( file != null ) {
-          IFile cabalFile = getCabalFile( file.getProject() );
-          result = fun.computeInfoHover( cabalFile, file, loc.getLine(), loc.getColumn() );
+    // XXX: this method does not get called when hovering over a string literal
+    // which leads to potential problem annotation hovers not appearing
+	  String hoverInfo = computeProblemInfo( textViewer, hoverRegion );
+	  if (hoverInfo != null) {
+	    return hoverInfo;
+	  }
+	  return computeThingAtPoint( textViewer, hoverRegion );
+  }
+
+  @SuppressWarnings ( "unchecked" )
+  private String computeProblemInfo( final ITextViewer textViewer, final IRegion hoverRegion ) {
+    if (textViewer instanceof ISourceViewer) {
+      IAnnotationModel annotationModel = ((ISourceViewer)textViewer).getAnnotationModel();
+      Iterator<Annotation> i = annotationModel.getAnnotationIterator();
+      while (i.hasNext()) {
+        Annotation a = i.next();
+        Position p = annotationModel.getPosition( a );
+        if (p.overlapsWith( hoverRegion.getOffset(), hoverRegion.getLength() )) {
+          return a.getText();
         }
-      } catch( BadLocationException ex ) {
-        // ignore, won't get any hover then
       }
     }
-    return result;
-    */
-	  IFile file = editor.findFile();
-	  if (file != null) {
-		  IDocument document = textViewer.getDocument();
-		  Location location;
-		  try {
-			  location = new Location(file.getLocation().toOSString(), document, hoverRegion);
-		  } catch (BadLocationException ex) {
-			  HaskellUIPlugin.log( UITexts.editor_textHover_error, ex );
-			  return null;
-		  }
+    return null;
+  }
+
+  private String computeThingAtPoint( final ITextViewer textViewer, final IRegion hoverRegion  ) {
+    IFile file = editor.findFile();
+    if (file != null) {
+      IDocument document = textViewer.getDocument();
+      Location location;
+      try {
+        location = new Location(file.getLocation().toOSString(), document, hoverRegion);
+      } catch (BadLocationException ex) {
+        HaskellUIPlugin.log( UITexts.editor_textHover_error, ex );
+        return null;
+      }
       ThingAtPointCommand command = new ThingAtPointCommand(location);
       Scion.syncRunCommand(command, 200);
       if (command.isSuccessful()) {
         return command.getThing();
       }
-      return null;
-	  }
-	  return null;
+    }
+    return null;
   }
 
   @Override
