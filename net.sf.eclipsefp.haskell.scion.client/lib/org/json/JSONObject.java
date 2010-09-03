fc70e98f2d98d4df47bb96ce40f6b8521784aa69
JP Moresmau <jp@moresmau.fr>
2010-09-03 14:24:50 +0200

    use unsynchronized StringBuilder

--- net.sf.eclipsefp.haskell.scion.client/lib/org/json/JSONObject.java
+++ net.sf.eclipsefp.haskell.scion.client/lib/org/json/JSONObject.java
@@ -1136,7 +1136,7 @@ public class JSONObject {
         char         c = 0;
         int          i;
         int          len = string.length();
-        StringBuffer sb = new StringBuffer(len + 4);
+        StringBuilder sb = new StringBuilder(len + 4);
         String       t;
 
         sb.append('"');
@@ -1326,7 +1326,7 @@ public class JSONObject {
     public String toString() {
         try {
             Iterator<String> keys = keys();
-            StringBuffer sb = new StringBuffer("{");
+            StringBuilder sb = new StringBuilder("{");
 
             while (keys.hasNext()) {
                 if (sb.length() > 1) {
@@ -1382,7 +1382,7 @@ public class JSONObject {
             return "{}";
         }
         Iterator<String> keys = sortedKeys();
-        StringBuffer sb = new StringBuffer("{");
+        StringBuilder sb = new StringBuilder("{");
         int          newindent = indent + indentFactor;
         Object       o;
         if (n == 1) {
