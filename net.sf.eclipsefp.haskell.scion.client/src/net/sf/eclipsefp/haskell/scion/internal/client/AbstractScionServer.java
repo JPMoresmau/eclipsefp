8bb8078edabcfe1db843150f1c73872030baec2b
Scott Michel <scottm@aero.org>
2010-10-12 13:48:37 -0700

    exeFactories_101012b
    Update documentation and reformat code to maintain style.

--- net.sf.eclipsefp.haskell.scion.client/src/net/sf/eclipsefp/haskell/scion/internal/client/AbstractScionServer.java
+++ net.sf.eclipsefp.haskell.scion.client/src/net/sf/eclipsefp/haskell/scion/internal/client/AbstractScionServer.java
@@ -16,95 +16,118 @@ import org.eclipse.swt.widgets.Display;
 
 /**
  * some helper code to implement a IScionServer
+ * 
  * @author JP Moresmau
- *
+ * 
  */
 public abstract class AbstractScionServer implements IScionServer {
-	protected static final String
-		CLASS_PREFIX = "[ScionServer]",
-		SERVER_STDOUT_PREFIX = "[scion-server]";
+  protected static final String CLASS_PREFIX         = "[ScionServer]";
+  protected static final String SERVER_STDOUT_PREFIX = "[scion-server]";
+  /**
+   * Path to the server executable that is started and with whom EclipseFP
+   * communicates
+   */
+  protected IPath               serverExecutable;
+  /** Server logging output stream, generally tends to be a Eclipse console */
+  protected Writer              serverOutput;
+  /** Working directory where the server operates, can be null if no project. */
+  protected File                directory;
+  /** The scion-server process */
+  protected Process             process;
+  /** Server's standard output */
+  protected BufferedReader      serverStdOutReader;
+  /** Request identifier */
+  private AtomicInteger         nextSequenceNumber = new AtomicInteger(1);
 
-	protected IPath serverExecutable;
-	protected Writer serverOutput;
-	
-	protected File directory;
-	
-	protected Process process;
-	protected BufferedReader serverStdOutReader;
+  /**
+   * The constructor
+   * 
+   * @param serverExecutable
+   *          The scion-server executable
+   * @param serverOutput
+   *          The scion-server's logging stream
+   * @param directory
+   *          The scion-server's working directory
+   */
+  public AbstractScionServer(IPath serverExecutable, Writer serverOutput, File directory) {
+    this.serverExecutable = serverExecutable;
+    this.serverOutput = serverOutput;
+    this.directory = directory;
+  }
 
-	private AtomicInteger nextSequenceNumber = new AtomicInteger(1);
-	
-	public AbstractScionServer(IPath serverExecutable, Writer serverOutput, File directory) {
-		this.serverExecutable = serverExecutable;
-		this.serverOutput=serverOutput;
-		this.directory=directory;
-	}
-	
-	protected int makeSequenceNumber() {
-		return nextSequenceNumber.getAndIncrement();
-	}
-	
-	/**
-	 * Reads a line from the server's stdout (or stderr).
-	 * Blocks until a line is available.
-	 * The returned line does not contain the newline character(s) at the end.
-	 * Returns null in case of EOF.
-	 */
-	protected String readLineFromServer() throws IOException {
-		final String line = serverStdOutReader.readLine();
-		if (line != null) {
-			if (serverOutput!=null){
-//				serverOutput.append(line+"\n");
-//				int ix=0;
-//				while (line.length()-ix>1024){
-//					serverOutput.append(line.substring(ix,ix+1024));
-//					//serverOutput.flush();
-//					ix+=1024;
-//				}
-//				serverOutput.append(line.substring(ix,line.length())+"\n");
-				// this seems to be what works the best to write to the console
-				// async in display, cut in chunks so it's not too big
-				try {
-					Display theDisplay = Display.getDefault();
-					theDisplay.asyncExec(new Runnable() {
-						public void run() {
-							try {
-								//serverOutput.append(line);
-								//serverOutput.append("\n");
-								//serverOutput.flush();
-								int ix=0;
-								while (line.length()-ix>1024){
-									serverOutput.append(line.substring(ix,ix+1024));
-									//serverOutput.flush();
-									ix+=1024;
-								}
-								serverOutput.append(line.substring(ix,line.length())+"\n");
-								serverOutput.flush();
-							} catch (IOException ioe){
-								// ignore
-							}
-						}
-					});
-				} catch (SWTException e) {
-					if (e.code == SWT.ERROR_THREAD_INVALID_ACCESS) {
-					  // UI thread is actually dead at this point, so ignore
-					  // (Note: This is actually semi-important on Mac OS X Cocoa.)
-					} else {
-					  // Re-throw the exception...
-					  throw e;
-					}
-				}
-			} else {
-				Trace.trace(SERVER_STDOUT_PREFIX, line);
-			}
-		} else {
-			if (serverOutput!=null){
-				serverOutput.append("Server gave EOF on stdout\n");
-				serverOutput.flush();
-			} else {
-				Trace.trace(CLASS_PREFIX, "Server gave EOF on stdout");
-			}
-		}
-		return line;
-	}
+  /**
+   * Get the next request identifier
+   * 
+   * @return The next atomic request number from {@link nextSequenceNumber
+   *         nextSequenceNumber}
+   */
+  protected int makeSequenceNumber() {
+    return nextSequenceNumber.getAndIncrement();
+  }
+
+  /**
+   * Reads a line from the server's stdout (or stderr), blocking until a line is
+   * available. The returned line does not contain the newline character(s) at
+   * the end.
+   * 
+   * @return The next full line of input read form the server, or null when EOF
+   *         is encountered.
+   */
+  protected String readLineFromServer() throws IOException {
+    final String line = serverStdOutReader.readLine();
+    if (line != null) {
+      if (serverOutput != null) {
+        // serverOutput.append(line+"\n");
+        // int ix=0;
+        // while (line.length()-ix>1024){
+        // serverOutput.append(line.substring(ix,ix+1024));
+        // //serverOutput.flush();
+        // ix+=1024;
+        // }
+        // serverOutput.append(line.substring(ix,line.length())+"\n");
+        // this seems to be what works the best to write to the console
+        // async in display, cut in chunks so it's not too big
+        try {
+          Display theDisplay = Display.getDefault();
+          theDisplay.asyncExec(new Runnable() {
+            public void run() {
+              try {
+                // serverOutput.append(line);
+                // serverOutput.append("\n");
+                // serverOutput.flush();
+                int ix = 0;
+                while (line.length() - ix > 1024) {
+                  serverOutput.append(line.substring(ix, ix + 1024));
+                  // serverOutput.flush();
+                  ix += 1024;
+                }
+                serverOutput.append(line.substring(ix, line.length()) + "\n");
+                serverOutput.flush();
+              } catch (IOException ioe) {
+                // ignore
+              }
+            }
+          });
+        } catch (SWTException e) {
+          if (e.code == SWT.ERROR_THREAD_INVALID_ACCESS) {
+            // UI thread is actually dead at this point, so ignore
+            // (Note: This is actually semi-important on Mac OS X Cocoa.)
+          } else {
+            // Re-throw the exception...
+            throw e;
+          }
+        }
+      } else {
+        Trace.trace(SERVER_STDOUT_PREFIX, line);
+      }
+    } else {
+      if (serverOutput != null) {
+        serverOutput.append("Server gave EOF on stdout\n");
+        serverOutput.flush();
+      } else {
+        Trace.trace(CLASS_PREFIX, "Server gave EOF on stdout");
+      }
+    }
+    return line;
+  }
 }
