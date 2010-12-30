6bca77d88f18d8f25926f153021d81161e59bcf9
Scott Michel <scottm@aero.org>
2010-12-29 22:33:46 -0800

    server_verbosity
    Hack in support for making scion-server less verbose, unless requested.

---
+++ net.sf.eclipsefp.haskell.scion.client/src/net/sf/eclipsefp/haskell/scion/client/IScionPreferenceNames.java
@@ -0,0 +1,11 @@
+package net.sf.eclipsefp.haskell.scion.client;
+
+/**
+ * Preference names related to scion-server client operation.
+ * 
+ * @author B. Scott Michel (scooter.phd@gmail.com)
+ */
+public interface IScionPreferenceNames {
+  /** Scion-server verbose interaction flag */
+  public final static String VERBOSE_INTERACTION = "verboseInteraction";
+}
