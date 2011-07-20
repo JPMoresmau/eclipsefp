e57789bb1abbc174402d2dd0c20bd13ee8c5f3a4
Alejandro Serrano <trupill@gmail.com>
2011-07-20 10:31:38 +0200

    Create profiler plug-in skeleton.

---
+++ net.sf.eclipsefp.haskell.profiler/src/net/sf/eclipsefp/haskell/profiler/internal/editors/ProfilerViewer.java
@@ -0,0 +1,58 @@
+package net.sf.eclipsefp.haskell.profiler.internal.editors;
+
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.IEditorInput;
+import org.eclipse.ui.IEditorSite;
+import org.eclipse.ui.PartInitException;
+import org.eclipse.ui.part.EditorPart;
+
+public class ProfilerViewer extends EditorPart {
+
+	public ProfilerViewer() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void doSave(IProgressMonitor monitor) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void doSaveAs() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void init(IEditorSite site, IEditorInput input) throws PartInitException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public boolean isDirty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isSaveAsAllowed() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void createPartControl(Composite parent) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setFocus() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
