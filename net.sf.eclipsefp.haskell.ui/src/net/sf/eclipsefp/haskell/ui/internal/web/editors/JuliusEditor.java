28126efb51da8ec30d82b5aef0e7fa8ad2d34aa3
serras <trupill@gmail.com>
2011-09-07 11:44:04 +0200

    Fix problems with web editors by subclassing StructuredTextEditor.

---
+++ net.sf.eclipsefp.haskell.ui/src/net/sf/eclipsefp/haskell/ui/internal/web/editors/JuliusEditor.java
@@ -0,0 +1,7 @@
+package net.sf.eclipsefp.haskell.ui.internal.web.editors;
+
+import org.eclipse.wst.sse.ui.StructuredTextEditor;
+
+public class JuliusEditor extends StructuredTextEditor {
+  // Nothing
+}
