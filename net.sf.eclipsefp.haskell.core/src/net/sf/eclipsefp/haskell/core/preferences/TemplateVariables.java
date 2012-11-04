48fefebc2e676e85474eff5804d40ebba2029c37
JP Moresmau <jp@moresmau.fr>
2012-11-04 21:46:45 +0100

    forgot new class

---
+++ net.sf.eclipsefp.haskell.core/src/net/sf/eclipsefp/haskell/core/preferences/TemplateVariables.java
@@ -0,0 +1,23 @@
+/**
+ * Copyright (c) 2012 by JP Moresmau
+ * This code is made available under the terms of the Eclipse Public License,
+ * version 1.0 (EPL). See http://www.eclipse.org/legal/epl-v10.html
+ */
+package net.sf.eclipsefp.haskell.core.preferences;
+
+/**
+ * Variables we define in the code templates
+ *
+ * @author JP Moresmau
+ *
+ */
+public interface TemplateVariables {
+
+  public static final String PROJECT_NAME="project_name"; //$NON-NLS-1$
+  public static final String USER_NAME="user_name"; //$NON-NLS-1$
+  public static final String SRC="src"; //$NON-NLS-1$
+  public static final String LIBRARY="library"; //$NON-NLS-1$
+  public static final String EXECUTABLE="executable"; //$NON-NLS-1$
+  public static final String MODULE="module"; //$NON-NLS-1$
+  public static final String MODULE_NAME="module_name"; //$NON-NLS-1$
+}
