d71d716c4da572739b095e156eb8dd8805c6afeb
jpmoresmau <jp@moresmau.fr>
2013-11-19 18:36:01 +0100

    delete markers before building

--- net.sf.eclipsefp.haskell.core/src/net/sf/eclipsefp/haskell/core/partitioned/uuagc/FullBuildVisitor.java
+++ net.sf.eclipsefp.haskell.core/src/net/sf/eclipsefp/haskell/core/partitioned/uuagc/FullBuildVisitor.java
@@ -1,5 +1,6 @@
 package net.sf.eclipsefp.haskell.core.partitioned.uuagc;
 
+import net.sf.eclipsefp.haskell.core.HaskellCorePlugin;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.resources.IResourceVisitor;
 import org.eclipse.core.runtime.CoreException;
@@ -14,6 +15,9 @@ public class FullBuildVisitor implements IResourceVisitor {
   @Override
   public boolean visit( final IResource resource ) throws CoreException {
     if( UuagcBuilder.mustBeVisited( resource ) ) {
+      // We have to clean the previous markers
+      resource.deleteMarkers( HaskellCorePlugin.ID_UUAGC_MARKER, true,
+          IResource.DEPTH_ZERO );
      UuagcBuilder.build( resource );
     }
     return true;
