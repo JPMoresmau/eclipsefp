995b3b71de4805dc0ed6b7984760782d2a036354
jpmoresmau <jp@moresmau.fr>
2015-02-09 20:40:03 +0100

    Cabal version drop down goes up to the currently used version of Cabal

--- net.sf.eclipsefp.haskell.ui/src/net/sf/eclipsefp/haskell/ui/internal/editors/cabal/forms/overview/CabalVersionChoice.java
+++ net.sf.eclipsefp.haskell.ui/src/net/sf/eclipsefp/haskell/ui/internal/editors/cabal/forms/overview/CabalVersionChoice.java
@@ -4,7 +4,11 @@
  */
 package net.sf.eclipsefp.haskell.ui.internal.editors.cabal.forms.overview;
 
+import java.util.ArrayList;
+import java.util.List;
+import net.sf.eclipsefp.haskell.core.cabal.CabalImplementationManager;
 import net.sf.eclipsefp.haskell.ui.internal.editors.cabal.forms.Choice;
+import org.osgi.framework.Version;
 
 /**
  * Possible versions of Cabal that may be set as minimal version required.
@@ -14,6 +18,21 @@ import net.sf.eclipsefp.haskell.ui.internal.editors.cabal.forms.Choice;
 public class CabalVersionChoice extends Choice<String> {
 
   private static String[] values = new String[] { "1.2", "1.4", "1.6", "1.8", "1.10", "1.12" };
+
+  static {
+    Version v=CabalImplementationManager.getCabalLibraryVersion();
+    if (v!=null){
+      int min=v.getMinor();
+      int max=v.getMajor();
+      List<String> vals=new ArrayList<>();
+      for (int a=1;a<=max;a++){
+        for (int b=2;b<=min;b+=2){
+          vals.add( a+"."+b );
+        }
+      }
+      values=vals.toArray( new String[vals.size()]);
+    }
+  }
 
   @Override
   public String[] getValues() {
